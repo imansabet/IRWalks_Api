3d3f0c27b9e48fa71153e3dcd31bed99019936f1
iman <dev.imansabet@gmail.com>
2024-08-16 23:02:57 +0330

    Create DbContext

---
+++ IRWalks.API/Data/IRWalksDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IRWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace IRWalks.API.Data;
+
+public class IRWalksDbContext : DbContext
+{
+    public IRWalksDbContext(DbContextOptions<IRWalksDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Walk> Walks { get; set; }
+    public DbSet<Difficulty> Difficulty { get; set; }
+}
