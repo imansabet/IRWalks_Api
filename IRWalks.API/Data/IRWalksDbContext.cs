2f7a1d247e5fb601372e75bb4feff3839e3bbaa3
iman <dev.imansabet@gmail.com>
2024-08-19 21:03:44 +0330

    Seed Data in Difficulty & Regions Table

--- IRWalks.API/Data/IRWalksDbContext.cs
+++ IRWalks.API/Data/IRWalksDbContext.cs
@@ -12,4 +12,66 @@ public class IRWalksDbContext : DbContext
     public DbSet<Region> Regions { get; set; }
     public DbSet<Walk> Walks { get; set; }
     public DbSet<Difficulty> Difficulty { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        var difficulties = new List<Difficulty>()
+        {
+            new Difficulty()
+            {
+                Id = Guid.Parse("98a39a48-78fa-41d7-ad97-445ccb84177c") ,
+                Name = "Easy",
+            },
+            new Difficulty()
+            {
+                Id = Guid.Parse("d817f8e9-5b09-4181-b064-8069b4b8fa61") ,
+                Name = "Medium",
+            },
+            new Difficulty()
+            {
+                Id = Guid.Parse("95ad1fcd-7876-4b09-be77-859c9e94a9b3") ,
+                Name = "Hard",
+            }
+        };
+        modelBuilder.Entity<Difficulty>().HasData(difficulties);
+
+        var regions = new List<Region>
+        {
+            new Region
+            {
+                Id = Guid.Parse("75a93ca7-7b5c-4038-b0c0-140952f70e4f"),
+                Name = "Fars",
+                Code = "FRS",
+                RegionImageUrl = "region.jpg",
+
+            },
+            new Region
+            {
+                Id = Guid.Parse("0a2c8388-f3cc-4328-9b3e-6f47b92c4a11"),
+                Name = "Tabriz",
+                Code = "TBZ",
+                RegionImageUrl = "region.jpg",
+
+            },
+            new Region
+            {
+                Id = Guid.Parse("10191792-8707-4768-9e95-7aa628328853"),
+                Name = "Tehran",
+                Code = "TEH",
+                RegionImageUrl = "region.jpg",
+
+            },
+            new Region
+            {
+                Id = Guid.Parse("5d860541-eb55-4aa7-9fd5-1c525f617788"),
+                Name = "Semnan",
+                Code = "SEM",
+                RegionImageUrl = "region.jpg",
+
+            },
+        };
+        modelBuilder.Entity<Region>().HasData(regions);
+
+    }
 }
--- IRWalks.API/Migrations/20240819171829_SeedRegionAndDifficulty.Designer.cs
+++ IRWalks.API/Migrations/20240819171829_SeedRegionAndDifficulty.Designer.cs
@@ -4,6 +4,7 @@ using IRWalks.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IRWalks.API.Migrations
 {
     [DbContext(typeof(IRWalksDbContext))]
-    partial class IRWalksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240819171829_SeedRegionAndDifficulty")]
+    partial class SeedRegionAndDifficulty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
