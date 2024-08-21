39c6873c08b02c6815cdd640ddde8541330d6351
iman <dev.imansabet@gmail.com>
2024-08-21 19:14:40 +0330

    Create identity db

--- IRWalks.API/Data/IRWalksAuthDbContext.cs
+++ IRWalks.API/Data/IRWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IRWalks.API.Data
@@ -6,8 +7,35 @@ namespace IRWalks.API.Data
     public class IRWalksAuthDbContext : IdentityDbContext
     {
         public IRWalksAuthDbContext(DbContextOptions<IRWalksAuthDbContext> options) : base(options)
+        {}
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-                
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "9002ed86-e1e7-4d82-9c07-5505719fec03";
+
+            var writerRoleId = "5cbcd6a1 - fc89 - 4cc6 - 870e-b8578dc4269c"; 
+
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
         }
+
     }
 }
