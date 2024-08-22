fbf407610b6c4f4358d7a60e6aa49ffde590a22e
iman <dev.imansabet@gmail.com>
2024-08-22 23:49:44 +0330

    Add Image Table To Db

--- IRWalks.API/Data/IRWalksDbContext.cs
+++ IRWalks.API/Data/IRWalksDbContext.cs
@@ -12,6 +12,7 @@ public class IRWalksDbContext : DbContext
     public DbSet<Region> Regions { get; set; }
     public DbSet<Walk> Walks { get; set; }
     public DbSet<Difficulty> Difficulty { get; set; }
+    public DbSet<Image> Images { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
