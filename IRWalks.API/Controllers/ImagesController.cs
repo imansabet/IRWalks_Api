8889b5683e4605b677b1318326c13568a8c492a5
iman <dev.imansabet@gmail.com>
2024-08-23 00:09:39 +0330

    add ImageController

---
+++ IRWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,37 @@
+﻿using IRWalks.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IRWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileUpload(request);
+            if (ModelState.IsValid)
+            {
+
+            }
+            return BadRequest(ModelState);
+
+        }
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if (!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsuppoerted extension for file");
+            }
+            if(request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "Filze size should be less than 10MB. ");
+
+            }
+        }
+    }
+}
